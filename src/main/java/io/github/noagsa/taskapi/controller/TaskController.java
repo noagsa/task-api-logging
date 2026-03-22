5daca96a0a1db4075a915dee83fc22bd8349e97f
Noa <noasalgadodev@gmail.com>
2026-03-22 21:52:10 +0100

    feat: add task controller

---
+++ src/main/java/io/github/noagsa/taskapi/controller/TaskController.java
@@ -0,0 +1,48 @@
+package io.github.noagsa.taskapi.controller;
+
+import io.github.noagsa.taskapi.dto.TaskRequestDTO;
+import io.github.noagsa.taskapi.dto.TaskResponseDTO;
+import io.github.noagsa.taskapi.service.TaskService;
+import jdk.dynalink.linker.LinkerServices;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tasks")
+public class TaskController {
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @PostMapping
+    public ResponseEntity<TaskResponseDTO> addTask(@RequestBody TaskRequestDTO taskRequestDTO) {
+        TaskResponseDTO createdTask = taskService.save(taskRequestDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdTask);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TaskResponseDTO>> getAllTasks() {
+        return ResponseEntity.ok().body(taskService.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TaskResponseDTO> getTask(@PathVariable long id) {
+        return ResponseEntity.ok().body(taskService.get(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<TaskResponseDTO> updateTask(@PathVariable long id, @RequestBody TaskRequestDTO taskRequestDTO) {
+        return ResponseEntity.ok().body(taskService.update(id, taskRequestDTO));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTask(@PathVariable long id) {
+        taskService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
